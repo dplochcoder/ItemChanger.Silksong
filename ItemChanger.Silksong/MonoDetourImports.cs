109174ba0e51118f05a6328f022706d86b27367b
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-03-27 17:50:06 +0000

    Add Taunt and novelty big UIDefs

--- ItemChanger.Silksong/MonoDetourImports.cs
+++ ItemChanger.Silksong/MonoDetourImports.cs
@@ -1,5 +1,7 @@
-﻿using MonoDetour.HookGen;
+﻿using HutongGames.PlayMaker.Actions;
+using MonoDetour.HookGen;
 
 [assembly: MonoDetourTargets(typeof(GameManager), GenerateControlFlowVariants = true)]
 [assembly: MonoDetourTargets(typeof(HeroController), GenerateControlFlowVariants = true)]
 [assembly: MonoDetourTargets(typeof(InventoryItemConditional))]
+[assembly: MonoDetourTargets(typeof(ListenForTauntV2), GenerateControlFlowVariants = true)]
--- ItemChanger.Silksong/Serialization/SpecialSprites/EvaHealSprite.cs
+++ ItemChanger.Silksong/Serialization/SpecialSprites/EvaHealSprite.cs
@@ -1,11 +1,13 @@
 ﻿using ItemChanger.Serialization;
 using ItemChanger.Silksong.Assets;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace ItemChanger.Silksong.Serialization.SpecialSprites;
 
 public class EvaHealSprite : IValueProvider<Sprite>
 {
+    [JsonIgnore]
     public Sprite Value => GameObjectKeys.ANCESTRAL_ART_GET_PROMPT
         .GetGameObjectPrefab()
         .GetComponent<PowerUpGetMsg>()
