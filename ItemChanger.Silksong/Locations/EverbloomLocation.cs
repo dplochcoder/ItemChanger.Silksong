f65a6e9ecf425669e1ef0534e65c5d573fdadf44
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-04-17 23:05:57 -0400

    Implement the Everbloom location.
    Closes #127.

---
+++ ItemChanger.Silksong/Locations/EverbloomLocation.cs
@@ -0,0 +1,20 @@
+﻿using HutongGames.PlayMaker.Actions;
+using ItemChanger.Locations;
+using Silksong.FsmUtil;
+
+namespace ItemChanger.Silksong.Locations;
+
+public class EverbloomLocation : AutoLocation
+{
+    protected override void DoLoad() => Using(new FsmEditGroup() { { new(SceneName!, "door_wakeInRedMemory Root", "Wake Up"), ModifyFsm } });
+
+    protected override void DoUnload() { }
+
+    private void ModifyFsm(PlayMakerFSM fsm)
+    {
+        var giveState = fsm.MustGetState("Get Item Msg");
+        giveState.GetFirstActionOfType<CreateUIMsgGetItem>()?.enabled = false;
+        giveState.GetFirstActionOfType<SetFsmString>()?.enabled = false;
+        giveState.AddMethod(_ => GiveAll(() => fsm.SendEvent("GET ITEM MSG END")));
+    }
+}
